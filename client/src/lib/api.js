5695ca5c726dc9d51ba38ffc24a432576496b5c4
TIBURONSIN67 <tiburonsin67@gmail.com>
2026-07-29 00:49:59 +0200

    Update API URL to new Render instance

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_URL = import.meta.env.PROD
-  ? 'https://paginaweb-l5qe.onrender.com/api'
+  ? 'https://paginaweb-1-jjlu.onrender.com/api'
   : '/api';
 
 const api = axios.create({
